63ee1ecbe55a3b5ccbf483ae6ae767fe19c57c6c
satele99 <satele.works@gmail.com>
2023-09-26 14:46:39 -0400

    new environment

--- zenbody-react-23/src/components/Footer.tsx
+++ zenbody-react-23/src/components/Footer.tsx
@@ -18,7 +18,7 @@ export default function Footer(){
                     <button type='submit'>Submit</button>
                 </form>
            </div>
-           
+           <p>©2023 by Cardinal Design, LLC</p>
         </footer>
     )
 }
